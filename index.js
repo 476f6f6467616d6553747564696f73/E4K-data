58eefb3fa13fcf2331bfe18e0193a91199cc591a
476f6f6467616d6553747564696f73 <109042424+476f6f6467616d6553747564696f73@users.noreply.github.com>
2026-05-13 15:11:12 +0200

    Add missing exports

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const {readdirSync} = require('fs');
 module.exports = {
     country: {},
     data: {},
+    imageDataX384: {},
     imageData: {},
     imageBaseUrl: "https://raw.githubusercontent.com/476f6f6467616d6553747564696f73/E4K-data/main/images/",
+    imageBaseUrlDev: "https://raw.githubusercontent.com/476f6f6467616d6553747564696f73/E4K-data/develop/images/",
     languages: {},
     network: {},
+    networkPreClient: {},
 }
 
 readdirSync(__dirname + '/country/').forEach(function (file) {
@@ -19,6 +22,7 @@ readdirSync(__dirname + '/data/').forEach(function (file) {
     module.exports.data[file.replace('.json', '')] = _data;
 });
 
+module.exports.imageDataX384 = require('./images/x384.json');
 module.exports.imageData = require('./images/x768.json');
 
 readdirSync(__dirname + '/languages/').forEach(function (file) {
@@ -26,3 +30,4 @@ readdirSync(__dirname + '/languages/').forEach(function (file) {
 });
 
 module.exports.network = require('./network/72.json');
+module.exports.networkPreClient = require('./network/77.json');
