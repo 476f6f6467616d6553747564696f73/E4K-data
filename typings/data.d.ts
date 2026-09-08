04f51c900d6a3f40faa550b55c2b78a018e07e22
476f6f6467616d6553747564696f73 <109042424+476f6f6467616d6553747564696f73@users.noreply.github.com>
2026-09-08 15:43:20 +0200

    4.141.29-785.RC.03

--- typings/data.d.ts
+++ typings/data.d.ts
@@ -507,11 +507,8 @@ export interface AllianceQuest {
     "comment4": string;
     "duration": number;
     "eventID"?: number;
-    "maxRaidBossLevel"?: number;
-    "minRaidBossLevel"?: number;
     "questID": number;
     "questType": string;
-    "raidBossID"?: number;
     "rewardPoints": number;
 }
 
@@ -2519,7 +2516,6 @@ export interface Package {
     "addRarePaint"?: number;
     "addRareRope"?: number;
     "addResourceVillageToken"?: number;
-    "addRiftQuestTicket"?: number;
     "addSaleDaysLuckyWheelTicket"?: number;
     "addSamuraiMedalBoosterKey"?: number;
     "addSamuraiToken"?: number;
