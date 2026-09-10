c8b5df4e10072607a420bea07080c31e52ef4301
476f6f6467616d6553747564696f73 <109042424+476f6f6467616d6553747564696f73@users.noreply.github.com>
2026-09-10 20:16:31 +0200

    4.141.33-785.RC.03

--- typings/languages.d.ts
+++ typings/languages.d.ts
@@ -774,6 +774,7 @@ export interface Language {
         "errorCode_379": string;
         "errorCode_393": string;
         "errorCode_397": string;
+        "errorCode_398": string;
         "errorCode_399": string;
         "errorCode_400": string;
         "errorCode_404": string;
@@ -1505,6 +1506,7 @@ export interface Language {
         "ci_secondary_unboostedFoodProduction_premium": string;
         "ci_secondary_unboostedHoneyProduction_premium": string;
         "ci_secondary_unboostedMeadProduction_premium": string;
+        "citadel": string;
         "coalboost_name": string;
         "coalboost_short_info": string;
         "coalboost_upgrade_info": string;
@@ -2748,6 +2750,7 @@ export interface Language {
         "bonusFusionXPChance_chance": string;
         "bossDungeon_attackAble_in": string;
         "btn_connectToFacebook": string;
+        "btn_join_bth2": string;
         "btn_login": string;
         "btn_play": string;
         "btn_send": string;
@@ -4612,6 +4615,28 @@ export interface Language {
         "dialog_bountyhunter_title_won": string;
         "dialog_bribetaxcollector2_copy": string;
         "dialog_bribetaxcollector_copy": string;
+        "dialog_bth2_allianceLimit_reached_desc": string;
+        "dialog_bth2_castleTransport_desc": string;
+        "dialog_bth2_castleTransport_startingLevel": string;
+        "dialog_bth2_castleTransport_subtitle": string;
+        "dialog_bth2_castleTransport_title": string;
+        "dialog_bth2_journeyProgress_title": string;
+        "dialog_bth2_main_allianceLimit_subtitle": string;
+        "dialog_bth2_main_journey_desc": string;
+        "dialog_bth2_main_journey_hiddenProgress": string;
+        "dialog_bth2_main_journey_subtitle": string;
+        "dialog_bth2_main_missingConditions_info": string;
+        "dialog_bth2_main_overview_desc": string;
+        "dialog_bth2_main_overview_title": string;
+        "dialog_bth2_preEnter_desc": string;
+        "dialog_bth2_start_desc1_desc": string;
+        "dialog_bth2_start_desc1_title": string;
+        "dialog_bth2_start_desc2_desc": string;
+        "dialog_bth2_start_desc2_title": string;
+        "dialog_bth2_start_desc3_desc": string;
+        "dialog_bth2_start_desc3_title": string;
+        "dialog_bth2_start_header": string;
+        "dialog_bth2_start_openHelpInfo": string;
         "dialog_bugreport_description": string;
         "dialog_bugreport_title": string;
         "dialog_bugreport_yourtext": string;
@@ -11288,6 +11313,7 @@ export interface Language {
         "loc_bladecoast_tower_Hidden": string;
         "loc_capital": string;
         "loc_castle": string;
+        "loc_citadel": string;
         "loc_dialog_underworld_finalboss": string;
         "loc_dialog_underworld_surrounding": string;
         "loc_dialog_underworld_tower": string;
@@ -20627,6 +20653,7 @@ export interface Language {
         "bribe_taxcollector": string;
         "bribe_taxcollector_copy": string;
         "bribe_taxcollector_tooltipp": string;
+        "bth2_eventName_short_title": string;
         "btn_discord_loginScreen_desc": string;
         "btn_ignoredPlayers": string;
         "btn_joinBeyondTheHorizon_Maya": string;
